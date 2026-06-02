e4de29422dacbaf5d79ec684c39e4738566ea8c5
Kurfun <masterp52888@gmail.com>
2026-06-03 03:03:24 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/chemecal-project/',
+  base: '/chemical-project2/',
   plugins: [vue()],
 })
